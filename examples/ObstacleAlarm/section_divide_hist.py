7467eea56ff879aea81c80bf3e685efc91059934
Boonthicha Saejia <boonthicha.s@gmail.com>
2022-03-21 22:20:36 +0700

    add new examples for ObstacleAlarm (hist and peak)

---
+++ examples/ObstacleAlarm/section_divide_hist.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+
+import cv2
+import depthai as dai
+import numpy as np
+
+def rankDepth(list_depth, n_iter=10):
+    arr_depth = np.array(list_depth)
+    bins = np.arange(0, 256, 15)
+    unique, counts = np.unique(np.digitize(arr_depth, bins), return_counts=True)
+    top_list = []
+    for i in range(n_iter):
+        max_idx = np.where(counts == np.max(counts))
+        top_list.append(max_idx[0][0])
+        counts[max_idx] = 0
+    results = np.array(top_list)*15
+    result  = np.max(results)
+    return result
+
+# Closer-in minimum depth, disparity range is doubled (from 95 to 190):
+extended_disparity = False
+# Better accuracy for longer distance, fractional disparity 32-levels:
+subpixel = False
+# Better handling for occlusions:
+lr_check = True
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define sources and outputs
+monoLeft = pipeline.create(dai.node.MonoCamera)
+monoRight = pipeline.create(dai.node.MonoCamera)
+depth = pipeline.create(dai.node.StereoDepth)
+xout = pipeline.create(dai.node.XLinkOut)
+
+xout.setStreamName("disparity")
+
+# Properties
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+
+# Create a node that will produce the depth map (using disparity output as it's easier to visualize depth this way)
+depth.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+# Options: MEDIAN_OFF, KERNEL_3x3, KERNEL_5x5, KERNEL_7x7 (default)
+depth.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
+depth.setLeftRightCheck(lr_check)
+depth.setExtendedDisparity(extended_disparity)
+depth.setSubpixel(subpixel)
+
+# Linking
+monoLeft.out.link(depth.left)
+monoRight.out.link(depth.right)
+depth.disparity.link(xout.input)
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+
+    # Output queue will be used to get the disparity frames from the outputs defined above
+    q = device.getOutputQueue(name="disparity", maxSize=4, blocking=False)
+    count = 0
+    top_head = 100
+    top_body = 200
+    top_leg = 300
+
+    while True:
+        count = count+1
+        inDisparity = q.get()  # blocking call, will wait until a new data has arrived
+        frame = inDisparity.getFrame()
+        # Normalization for better visualization
+        frame = (frame * (255 / depth.initialConfig.getMaxDisparity())).astype(np.uint8)
+        # if count == 1:
+            #frame[400x640] = height x width
+        for i in range(1,9):
+            frame = cv2.line(frame, (int(640/9*i), 0),(int(640/9*i), 400), (255, 0, 0), thickness=1)
+        frame = cv2.line(frame, (0, top_head),(640, top_head), (255, 0, 0), thickness=1)
+        frame = cv2.line(frame, (0, top_body),(640, top_body), (255, 0, 0), thickness=1)
+        frame = cv2.line(frame, (0, top_leg), (640, top_leg),  (255, 0, 0), thickness=1)
+
+        cv2.imshow("disparity", frame)
+
+        warning_section = np.zeros((3, 9))
+        width = 640
+        width_section = 9
+        threshold = 0
+        n_iter = 1
+        for i in range(9):
+            lst_depth = []
+            rep_depth = 0
+            for j in range(int(width/width_section*i),int(width/width_section*(i+1))):
+                for k in range(top_head,top_body):
+                    if (frame[k][j] > threshold ):
+                        lst_depth.append(frame[k][j])
+            if len(lst_depth)==0:
+                rep_depth = 255
+            else:
+                result = rankDepth(lst_depth, n_iter)
+                rep_depth = int(result)
+            warning_section[0][i] = rep_depth
+
+            lst_depth = []
+            rep_depth = 0
+            for j in range(int(width / width_section * i), int(width / width_section * (i + 1))):
+                for k in range(top_body,top_leg):
+                    if (frame[k][j] > threshold):
+                        lst_depth.append(frame[k][j])
+            if len(lst_depth) == 0:
+                rep_depth = 255
+            else:
+                result = rankDepth(lst_depth, n_iter)
+                rep_depth = int(result)
+                # lst_rank = rankDepth(lst_depth, n_iter)
+                # rep_depth = int(sum(lst_rank) / n_iter)
+            warning_section[1][i] = rep_depth
+
+            lst_depth = []
+            rep_depth = 0
+            for j in range(int(width / width_section * i), int(width / width_section * (i + 1))):
+                for k in range(top_leg,400):
+                    if (frame[k][j] > threshold ):
+                        lst_depth.append(frame[k][j])
+            if len(lst_depth) == 0:
+                rep_depth = 255
+            else:
+                result = rankDepth(lst_depth, n_iter)
+                rep_depth = int(result)
+                # lst_rank = rankDepth(lst_depth, n_iter)
+                # rep_depth = int(sum(lst_rank) / n_iter)
+            warning_section[2][i] = rep_depth
+        print(warning_section)
+
+        whiteblankimage = 255 * np.ones(shape=[400, 640, 3], dtype=np.uint8)
+        for i in range(3):
+            for j in range(9):
+                if warning_section[i][j] <= 15:
+                    cv2.rectangle(whiteblankimage, pt1=(int(640/9)*j, (100*i)+100), pt2=(int(640/9)*(j+1), (100*i)+200), color=(224, 253, 222),thickness=-1)
+                elif warning_section[i][j] <= 45:
+                    cv2.rectangle(whiteblankimage, pt1=(int(640/9)*j, (100*i)+100), pt2=(int(640/9)*(j+1), (100*i)+200), color=(222, 247, 252), thickness=-1)
+                elif warning_section[i][j] <= 105:
+                    cv2.rectangle(whiteblankimage, pt1=(int(640/9)*j, (100*i)+100), pt2=(int(640/9)*(j+1), (100*i)+200), color=(211, 223, 255), thickness=-1)
+                else:
+                    cv2.rectangle(whiteblankimage, pt1=(int(640/9)*j, (100*i)+100), pt2=(int(640/9)*(j+1), (100*i)+200), color=(162, 154, 255), thickness=-1)
+        cv2.imshow("warning_color", whiteblankimage)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+
+
+
+
+
+
+
+
+
+        # Available color maps: https://docs.opencv.org/3.4/d3/d50/group__imgproc__colormap.html
+        frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
+        # if count == 1:
+            #frame[400x640x3] = height x width x color
+        for i in range(1,9):
+            frame = cv2.line(frame, (int(640/9*i), 0),(int(640/9*i), 400), (255, 255, 255), thickness=1)
+        frame = cv2.line(frame, (0, top_head),(640, top_head), (255, 255, 255), thickness=1)
+        frame = cv2.line(frame, (0, top_body),(640, top_body), (255, 255, 255), thickness=1)
+        frame = cv2.line(frame, (0, top_leg), (640, top_leg),  (255, 255, 255), thickness=1)
+        cv2.imshow("disparity_color", frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+
+
+
